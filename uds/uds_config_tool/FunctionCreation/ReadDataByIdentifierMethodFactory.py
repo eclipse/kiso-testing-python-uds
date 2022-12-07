3da8747cf184d51220066cd7c0969f62837346e7
Jonas <32525495+JoSglch@users.noreply.github.com>
2022-12-07 11:50:35 +0100

    fix: change log level on ODX parsing exception to debug (#13)

--- uds/uds_config_tool/FunctionCreation/ReadDataByIdentifierMethodFactory.py
+++ uds/uds_config_tool/FunctionCreation/ReadDataByIdentifierMethodFactory.py
@@ -213,7 +213,7 @@ class ReadDataByIdentifierMethodFactory(IServiceMethodFactory):
                                 nrcElem.find("COMPU-CONST").find("VT").text
                             )
                     except Exception as e:
-                        log.warning(f"Exception while parsing ODX in checkNegativeResponse: {e}")
+                        log.debug(f"Exception while parsing ODX in checkNegativeResponse: {e}")
                 pass
 
         negativeResponseFunctionString = negativeResponseFuncTemplate.format(
